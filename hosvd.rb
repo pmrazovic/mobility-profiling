94aebfe23bc0e1941655de7a5846b32562c2f855
Petar Mrazovic <petar.mrazovic@gmail.com>
2015-04-15 13:25:54 +0200

    Implemented dimensionality reduction for constructed tensors.

--- hosvd.rb
+++ hosvd.rb
@@ -3,97 +3,122 @@
 require "narray"
 require 'numru/lapack'
 
-def kronecker_product(a,b)
-  result = NArray.float(a.shape[0]*b.shape[0],a.shape[1]*b.shape[1])
-  (0..a.shape[0]-1).each do |j|
-    (0..a.shape[1]-1).each do |i|
-      result[j*b.shape[0]..(j*b.shape[0]+b.shape[0]-1),i*b.shape[1]..(i*b.shape[1]+b.shape[1]-1)] = a[j,i]*b
+module HOSVD
+
+  public 
+
+  def self.run(tensor)
+    mode_matrices = []
+    n_mode_singular_values = []
+    (0..2).each do |axis|
+      s, u, vt, work, info, a = NumRu::Lapack.dgesvd( 'A', 'N', unfold(tensor,axis).transpose(1,0))
+      mode_matrices << u.transpose(1,0)
+      n_mode_singular_values << s
     end
+    
+    # number of principal components
+    c1 = 0.5
+    c2 = 0.5
+    c3 = 0.8
+
+    # c-dimensionally reduced mode matrices
+    u1 = mode_matrices[0][0..(mode_matrices[0].shape[0]*c1).ceil-1,true]
+    u2 = mode_matrices[1][0..(mode_matrices[1].shape[0]*c2).ceil-1,true]
+    u3 = mode_matrices[2][0..(mode_matrices[2].shape[0]*c3).ceil-1,true]
+
+    # core tensor unfolded along axis 0
+    s_u = NMatrix.ref(u1).transpose(1,0)*NMatrix.ref(unfold(tensor,0)) * NMatrix.ref(kronecker_product(u2,u3))
+    tensor_reconstructed_u = NMatrix.ref(u1)*s_u*NMatrix.ref(kronecker_product(u2,u3).transpose(1,0))
+
+    # checking results
+    # puts NMatrix.ref(unfold(tensor,0)).inspect
+    # puts (NMatrix.ref(u1)*s_u*NMatrix.ref(kronecker_product(u2,u3).transpose(1,0))).inspect
+
+    return fold(tensor_reconstructed_u,0,tensor.shape)
   end
-  return result
-end
 
-def unfold(tensor,axis)
-  # NArray follows FORTRAN index ordering (I_2 (axis_2) x I_1 (axis_1) x I_3 (axis_3))
+  private
 
-  dim = tensor.shape
-
-  if axis == 0
-    a_u = NArray.float(dim[0]*dim[2],dim[1])
-    count = 0
-    (0..dim[0]-1).each do |idx0|
-      (0..dim[2]-1).each do |idx2|
-        a_u[count,true] = tensor[idx0,true,idx2]
-        count += 1
-      end      
+  def self.kronecker_product(a,b)
+    result = NArray.float(a.shape[0]*b.shape[0],a.shape[1]*b.shape[1])
+    (0..a.shape[0]-1).each do |j|
+      (0..a.shape[1]-1).each do |i|
+        result[j*b.shape[0]..(j*b.shape[0]+b.shape[0]-1),i*b.shape[1]..(i*b.shape[1]+b.shape[1]-1)] = a[j,i]*b
+      end
     end
-  elsif axis == 1
-    a_u = NArray.float(dim[1]*dim[2],dim[0])
-    count = 0
-    (0..dim[1]-1).each do |idx1|
+    return result
+  end
+
+  def self.unfold(tensor,axis)
+    # NArray follows FORTRAN index ordering (I_2 (axis_2) x I_1 (axis_1) x I_3 (axis_3))
+
+    dim = tensor.shape
+
+    if axis == 0
+      a_u = NArray.float(dim[0]*dim[2],dim[1])
+      count = 0
+      (0..dim[0]-1).each do |idx0|
+        (0..dim[2]-1).each do |idx2|
+          a_u[count,true] = tensor[idx0,true,idx2]
+          count += 1
+        end      
+      end
+    elsif axis == 1
+      a_u = NArray.float(dim[1]*dim[2],dim[0])
+      count = 0
       (0..dim[2]-1).each do |idx2|
-        a_u[count,true] = tensor[true,idx1,idx2]
-        count += 1
-      end      
-    end
-  elsif axis == 2
-    a_u = NArray.float(dim[0]*dim[1],dim[2])
-    count = 0
-    (0..dim[0]-1).each do |idx0|
+        (0..dim[1]-1).each do |idx1|
+          a_u[count,true] = tensor[true,idx1,idx2]
+          count += 1
+        end      
+      end
+    elsif axis == 2
+      a_u = NArray.float(dim[0]*dim[1],dim[2])
+      count = 0
       (0..dim[1]-1).each do |idx1|
-        a_u[count,true] = tensor[idx0,idx1,true]
-        count += 1
-      end      
+        (0..dim[0]-1).each do |idx0|
+          a_u[count,true] = tensor[idx0,idx1,true]
+          count += 1
+        end      
+      end
     end
+
+    return a_u
   end
 
-  return a_u
-end
+  def self.fold(tensor_unfold,axis,shape)
+    # NArray follows FORTRAN index ordering (I_2 (axis_2) x I_1 (axis_1) x I_3 (axis_3))
 
-def fold(matrix,axis)
-  # NArray follows FORTRAN index ordering (I_2 (axis_2) x I_1 (axis_1) x I_3 (axis_3))
-end
+    # desired shape of folded tensor
+    tensor = NArray.float(shape[0],shape[1],shape[2])
 
-def hosvd(tensor)
-  mode_matrices = []
-  n_mode_singular_values = []
-  (0..2).each do |axis|
-    s, u, vt, work, info, a = NumRu::Lapack.dgesvd( 'A', 'A', unfold(tensor,axis).transpose(1,0))
-    mode_matrices << u.transpose(1,0)
-    n_mode_singular_values << s
+    if axis == 0
+      (0..shape[1]-1).each do |idx1|
+        (0..shape[0]-1).each do |idx0|
+          (0..shape[2]-1).each do |idx2|
+            tensor[idx0,idx1,idx2] = tensor_unfold[idx0*shape[2]+idx2,idx1]
+          end
+        end
+      end
+    elsif axis == 1
+      (0..shape[0]-1).each do |idx0|
+        (0..shape[2]-1).each do |idx2|
+          (0..shape[1]-1).each do |idx1|
+            tensor[idx0,idx1,idx2] = tensor_unfold[idx2*shape[1]+idx1,idx0]
+          end
+        end
+      end
+    elsif axis == 2
+      (0..shape[2]-1).each do |idx2|
+        (0..shape[1]-1).each do |idx1|
+          (0..shape[0]-1).each do |idx0|
+            tensor[idx0,idx1,idx2] = tensor_unfold[idx1*shape[0]+idx0,idx2]
+          end
+        end
+      end
+    end
+
+    return tensor
   end
-  
-  # number of principal components
-  c1 = 0.5
-  c2 = 0.5
-  c3 = 0.5
 
-  # c-dimensionally reduced mode matrices
-  u1 = mode_matrices[0][0..(mode_matrices[0].shape[0]*c1).ceil,true]
-  u2 = mode_matrices[1][0..(mode_matrices[1].shape[0]*c2).ceil,true]
-  u3 = mode_matrices[2][0..(mode_matrices[2].shape[0]*c3).ceil,true]
-
-  # core tensor unfolded along axis 0
-  s_u = NMatrix.ref(u1).transpose(1,0)*NMatrix.ref(unfold(tensor,0)) * NMatrix.ref(kronecker_product(u2,u3))
-
-  # checking results
-  puts NMatrix.ref(unfold(tensor,0)).inspect
-  puts (NMatrix.ref(u1)*s_u*NMatrix.ref(kronecker_product(u2,u3).transpose(1,0))).inspect
-
-  return s_u
 end
-
-
-# example tensor
-t = NArray[ [ [ 1.0, 2.0 ], 
-              [ 1.0, 2.0 ], 
-              [ 2.0, 4.0 ] ], 
-            [ [ 1.0, 2.0 ], 
-              [ -1.0, -2.0 ], 
-              [ 0.0, 0.0 ] ], 
-            [ [ 0.0, 0.0 ], 
-              [ 2.0, 4.0 ], 
-              [ 2.0, 4.0 ] ] ]
-              
-
-hosvd(t)
--- stay_point_clustering.rb
+++ stay_point_clustering.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require './optics'
 require "./hierarchical_clustering_processor"
+require "./hosvd"
 
 # reading predescovered stay points
 rows = []
@@ -32,86 +33,95 @@ finish = Time.now
 puts "Finished in #{(finish - start).round(2)} seconds"
 puts "-------------------------------------"
 
-# Initialize empty user structures wrt framework
-users = []
-(0..181).each{ |idx| users << [] }
-framework.each_with_index do |level,level_idx|
-  users.each{|u| u << [] }
-  level.each_with_index do |cluster, cluster_idx|
-    users.each{|u| u[level_idx] << [] }
-  end
-end
 
-# Populate user structures
-framework.each_with_index do |level,level_idx|
+# Constructing tensors
+puts "Constructing tensors..."
+start = Time.now
+
+# Defining time frames for temporal analysis
+time_frames = [ {:start => 0, :end => 3},
+                {:start => 3, :end => 6},
+                {:start => 6, :end => 9},
+                {:start => 9, :end => 12},
+                {:start => 12, :end => 15},
+                {:start => 15, :end => 18},
+                {:start => 18, :end => 21},
+                {:start => 21, :end => 24} ]
+
+# tensor array (one tensor per level - tensors[level_idx])
+tensors = Array.new
+# constructing tensors (user x cluster x time_frame)
+framework.each do |level|
+  tensor = NArray.float(level.size,182,8)
   level.each_with_index do |cluster, cluster_idx|
-    cluster.each do |point|
-      users[point[0]][level_idx][cluster_idx] << point[1..-1]
+    cluster.each do |stay_point|
+      # calculating stay time in each time frame
+      time_frames.each_with_index do |time_frame, time_frame_idx|
+        time = 0
+        arrival = stay_point[3]
+        departure = stay_point[4]
+
+        # stays can happen through midnight which complicates calculation,
+        # i.e. we need to split stay in two intervals (before and after midnight)
+        stays = []
+        if arrival.hour <= departure.hour
+          stays << { :arrival => {:hour => arrival.hour, :min => arrival.min, :sec => arrival.sec}, 
+                     :departure => {:hour => departure.hour, :min => departure.min, :sec => departure.sec} }
+        elsif arrival.hour > departure.hour
+          stays << { :arrival => {:hour => arrival.hour, :min => arrival.min, :sec => arrival.sec}, 
+                     :departure => {:hour => 24, :min => 0, :sec => 0} }
+          stays << { :arrival => {:hour => 0, :min => 0, :sec => 0}, 
+                     :departure => {:hour => departure.hour, :min => departure.min, :sec => departure.sec} }
+        end
+
+        # calculating time per frame
+        stays.each do |stay|
+          if stay[:arrival][:hour] >= time_frame[:start] && stay[:departure][:hour] < time_frame[:end]
+            time = (stay[:departure][:hour]*60 + stay[:departure][:min] + stay[:departure][:sec]/60.0) -
+                   (stay[:arrival][:hour]*60 + stay[:arrival][:min] + stay[:arrival][:sec]/60.0)
+          elsif stay[:arrival][:hour] < time_frame[:start] && stay[:departure][:hour] >= time_frame[:start] && stay[:departure][:hour] < time_frame[:end]
+            time = (stay[:departure][:hour]*60 + stay[:departure][:min] + stay[:departure][:sec]/60.0) - time_frame[:start]*60
+          elsif stay[:arrival][:hour] >= time_frame[:start] && stay[:arrival][:hour] < time_frame[:end] && stay[:departure][:hour] >= time_frame[:end]
+            time = time_frame[:end]*60 - (stay[:arrival][:hour]*60 + stay[:arrival][:min] + stay[:arrival][:sec]/60.0) 
+          elsif stay[:arrival][:hour] < time_frame[:start] && stay[:departure][:hour] >= time_frame[:end]
+            time = (time_frame[:end] - time_frame[:start])*60
+          end
+        end
+
+        # updating corresponding tensor value
+        tensor[cluster_idx,stay_point[0],time_frame_idx] += time
+      end
     end
   end
-end
 
-# Generating feature vectors for each level
-feature_vectors = []
-framework.each_with_index do |level,level_idx|
-  feature_vectors << []
-  users.each do |user|
-    user_vector = []
-    user[level_idx].each do |cluster|
-      sum = 0
-      cluster.each{|p| sum += p[-1] }
-      user_vector << sum
-    end
-    feature_vectors[level_idx] << user_vector
-  end
+  tensors << tensor
 end
+puts "Finished in #{(finish - start).round(2)} seconds"
+puts "-------------------------------------"
 
-def cosine_similarity(user_idx,a,m)
-  dot_prods = (m*a).sum(0)
-  magn_prods = NMath.sqrt((m**2).sum(0))*NMath.sqrt((a**2).sum(0))
-  # preventing devision by zero
-  zero_positions = magn_prods.eq(0).where
-  sim_vector = dot_prods/magn_prods
-  sim_vector[zero_positions] = 0.0
-  sim_vector[user_idx] = 1.0
-  return sim_vector
-end
+# Running HOSVD
+puts "Running HOSVD..."
+start = Time.now
+tensors_prime = []
+tensors.each{|tensor| tensors_prime << HOSVD.run(tensor)}
+finish = Time.now
+puts "Finished in #{(finish - start).round(2)} seconds"
+puts "-------------------------------------"
 
-similarity_matrices = []
-feature_vectors.each do |level_feature_matrix|
-  level_sim_matrix = []
-  level_feature_matrix.each_with_index do |feature_vector, feature_vector_idx|
-    level_sim_matrix << cosine_similarity(feature_vector_idx, NArray.to_na(feature_vector), NArray.to_na(level_feature_matrix))
-  end
-  similarity_matrices << NArray.to_na(level_sim_matrix)
-end
-
-accumulated_sim_matrix = similarity_matrices[0]
-weight = 1
-similarity_matrices.each_with_index do |level_sim_matrix, idx|
-  next if idx == 0
-  accumulated_sim_matrix += level_sim_matrix*(2**idx)
-  weight += 2**idx
-end
-accumulated_sim_matrix /= weight
-
-puts similarity_matrices.inspect
-puts accumulated_sim_matrix.inspect
-
-# Printing in Matlab file
-def print_level(level, x)
-  file = File.open("result.m","w")
-  file.puts "\nformat long"
-  file.puts "hold all"
-  level.each do |cluster|
-    file.puts "lat = [#{cluster.to_a.collect{|p| p[1]}.join(" ")}]"
-    file.puts "lon = [#{cluster.to_a.collect{|p| p[2]}.join(" ")}]"
-    file.puts "k=convhull(lon,lat);"
-    file.puts "plot(lon,lat,'.','MarkerSize',20)"
-    file.puts "plot(lon(k),lat(k),'-b')"
-  end
-  file.puts "plot_google_map"
-end
+# # Printing in Matlab file
+# def print_level(level, x)
+#   file = File.open("result.m","w")
+#   file.puts "\nformat long"
+#   file.puts "hold all"
+#   level.each do |cluster|
+#     file.puts "lat = [#{cluster.to_a.collect{|p| p[1]}.join(" ")}]"
+#     file.puts "lon = [#{cluster.to_a.collect{|p| p[2]}.join(" ")}]"
+#     file.puts "k=convhull(lon,lat);"
+#     file.puts "plot(lon,lat,'.','MarkerSize',20)"
+#     file.puts "plot(lon(k),lat(k),'-b')"
+#   end
+#   file.puts "plot_google_map"
+# end
 
 # Comparing two users using Matlab plot
 
